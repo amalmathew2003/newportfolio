9b0b5c6a2c2b6e6389059bdee81a80ff1aea97b7
mathe <mathewamalmathew@gmail.com>
2026-01-21 13:49:27 +0530

    feat: Set up initial Flutter application structure and add a commented-out mouse follower widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:my_portfolio/screen/main_page.dart';
-import 'package:my_portfolio/widgets/mouse_follower.dart';
 
 void main() {
   runApp(const PortfolioApp());
@@ -13,7 +12,7 @@ class PortfolioApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MouseFollower(child: PortfolioLandingPage()),
+      home: PortfolioLandingPage(),
     );
   }
 }
