5d608c38648bc9235859752626a4a6dab6ee5821
amalmathew2003 <amalmathew2003@users.noreply.github.com>
2026-01-29 04:34:43 +0000

    deploy: 54c1253b5c854d7c05a9873abc37b69c11ea5aa9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1743189271"
+    serviceWorkerVersion: "1845278235"
   }
 });
