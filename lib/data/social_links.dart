f968f02c77d8c26c4bd04cba4d2be504f496042e
mathe <mathewamalmathew@gmail.com>
2026-08-20 09:57:39 +0530

    cv link updated

--- lib/data/social_links.dart
+++ lib/data/social_links.dart
@@ -7,5 +7,5 @@ class SocialLinks {
       'https://www.linkedin.com/in/amal-mathew-1-/';
   static const String githubUrl = 'https://github.com/amalmathew2003';
   static const String cvDownloadUrl =
-      'https://drive.google.com/uc?export=download&id=1KsdjZsEGs80qGyRYwWDUKo5bkkmOTUF0';
+      'https://drive.google.com/uc?export=download&id=1AlWNKivLKhe6ARUisE78amwfgH5cbBvb';
 }
